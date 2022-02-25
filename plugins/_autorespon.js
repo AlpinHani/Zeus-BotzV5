4dafda4f81c63e04144ca520f6dbb6428a70eb8e
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-02-25 19:04:34 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -15,7 +15,7 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'AlpinXD tidak aktif' : banned ? 'kamu dibanned' : 'Bot  disini',
+                isBanned ? 'AlpinXD tidak aktif' : banned ? 'kamu dibanned' : 'AlpinXD disini, ciee tag pas butuh doang',
                 '©AlpinXD',
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
