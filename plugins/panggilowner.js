d54c56e0e9f2e3f2017629484f94cd74d0e345ca
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-02-27 07:45:14 +0700

    Update panggilowner.js

--- plugins/panggilowner.js
+++ plugins/panggilowner.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./src/tag.webp')
-conn.fakeReply(m.chat, stc, '6282287750102@s.whatsapp.net', '*Kenapa Tag Owner Saya*')
+conn.fakeReply(m.chat, stc, '6282287750102@s.whatsapp.net', '*TUMBEN TAG OWNER >_<*')
 }
 handler.customPrefix = /^(@6282287750102)$/i
 handler.command = new RegExp
