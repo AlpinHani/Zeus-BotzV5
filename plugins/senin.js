7c52a6e30cce2ba64ff8de6489de351a99899fb2
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-02-27 07:38:17 +0700

    Update senin.js

--- plugins/senin.js
+++ plugins/senin.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/besok-senin.webp')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*≥﹏≤*', 'status@broadcast')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*BESOK SENIN*', 'status@broadcast')
 }
 
 handler.customPrefix = /^(senin|Senin|Besok Senin|Besok senin|besok senin|besok Senin)$/i
