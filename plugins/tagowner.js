1a82747552b36c5032ac7492f29c31af231bc84c
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-11 17:09:41 +0700

    Create tagowner.js

---
+++ plugins/tagowner.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, text }) => {
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *@6282287750102 Ada yang memanggil anda 🗿*`, m)
+
+  conn.reply(m.chat, `
+ _Kak_ @6282287750102 *_Lagi Sibuk_* _-
+`.trim(), m)
+    let mentionedJid = [m.sender]
+}
+handler.customPrefix = /@6282287750102 /i
+handler.command = new RegExp
+
+module.exports = handler
