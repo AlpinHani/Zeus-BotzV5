c0fa5a18345dc47c3eebe9023604c5d11297998e
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-11 17:04:28 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,13 +1,19 @@
-let handler = async (m, { text }) => {
+let handler = (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`
-${conn.getName(m.sender)} sekarang AFK${text ? ': ' + text : ''}
-`)
+  let str = `╭──[ *NOW AFK !* ]──✧
+┆ *Nama* : ${conn.getName(m.sender)}
+┆ *Alasan* : ${text ? '' + text : ''}
+╰┅────★
+`.trim()
+conn.sendButton(m.chat, str, wm, 'Jangan Ganggu Ya Kak!', 'iyaaaaa',m)
+conn.reply(str)
 }
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
+handler.help = ['afk <alasan>']
+handler.tags = ['group']
 handler.command = /^afk$/i
 
 module.exports = handler
+
+let wm = global.botwm
