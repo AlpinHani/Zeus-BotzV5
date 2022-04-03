de4e07120895f7813189b1dbd2185e6ba3c691eb
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-04-03 11:13:49 +0700

    Update ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -1,12 +1,12 @@
 let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
-  let res = await fetch('https://api.xteam.xyz/randomimage/ppcouple?APIKEY=cristian9407')
+  let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'cowo', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'cewe', m)
+  await conn.sendFile(m.chat, json.result.male, '', 'Cowo', m)
+  await conn.sendFile(m.chat, json.result.female, '', 'Cewe', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet', 'anime']
